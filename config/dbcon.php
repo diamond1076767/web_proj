df14804c30cf7ae94c5ae1d5361bf6fc2dcacceb
TrouLZ <xieyujun2005@gmail.com>
2026-03-29 16:18:00 +0800

    u added the db header but did not include it inside dbcon.php.

--- config/dbcon.php
+++ config/dbcon.php
@@ -1,15 +1,18 @@
 <?php
 
-$env = parse_ini_file('../.env');
+$config = parse_ini_file(__DIR__ . '/../.env', true);
 
-$db_hostname=$env['DB_SERVER'];
-$db_username=$env['USERNAME'];
-$db_password=$env['PASSWORD'];
-$db_database=$env['DB_NAME'];
-
-$con = mysqli_connect($db_hostname,$db_username,$db_password,$db_database);
-if(!$con){
-    die("Connection Failed: ".mysqli_connect_error());
+if (!$config) {
+    die(".env file not found. Please create one in project root.");
 }
 
-?>
+$db_host = $config['database']['DB_SERVER'] ?? 'localhost';
+$db_user = $config['database']['USERNAME'] ?? 'root';
+$db_pass = $config['database']['PASSWORD'] ?? '';
+$db_name = $config['database']['DB_NAME'] ?? '';
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$con) {
+    die("Connection Failed: " . mysqli_connect_error());
+}
